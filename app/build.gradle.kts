c968e58578c14b5fdfc5d902ca21247007347d33
cacko <alex@cacko.net>
2026-03-17 17:44:07 +0200

    Update versionCode to 3
    Increment the application `versionCode` from 2 to 3 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "net.cacko.watts"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
