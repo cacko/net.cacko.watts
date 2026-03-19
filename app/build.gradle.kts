24445e0339df7cf0ce28a9f5759b2b78c631f49f
cacko <alex@cacko.net>
2026-03-19 13:43:49 +0200

    Bump versionCode to 6
    Increment the application version code from 5 to 6 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "net.cacko.watts"
         minSdk = 35
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
